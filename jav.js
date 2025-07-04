ce9960dfe8028a8168fe91474e7251142f79d017
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-07-04 18:07:31 +0700

    made optimizations

--- jav.js
+++ jav.js
@@ -27,30 +27,31 @@ function getPlayerChoice(){
     else {
         choice=prompt("Invalid, try again")
     }
-     choice=prompt("Invalid, try again")
     }
     return choice
 }
 function playRound(getComputerChoice,getPlayerChoice){
     if (getComputerChoice===getPlayerChoice){
-        console.log("It's a tie!!!")
+        console.log("It's a tie!")
     }
     else if (getComputerChoice === "Paper" && getPlayerChoice==="Rock" || getComputerChoice === "Rock" && getPlayerChoice==="Scissors" || getComputerChoice === "Scissors" && getPlayerChoice==="Paper") {
-        console.log("Computer wins!!!!!")
+        console.log("Computer wins!")
         ++computerScore
     }
     else{
-        console.log("You win!!!!!")
+        console.log("You win!")
         ++humanScore
     }
+    console.log(`${humanScore} - ${computerScore}`)
 }
 function playGame(){
 for (let i =1; i<=5; i++){
-console.log(playRound(getComputerChoice(),getPlayerChoice()))
-console.log(humanScore)
-console.log(computerScore)
+playRound(getComputerChoice(),getPlayerChoice())
+}
+}
+playGame()
 if (humanScore===computerScore){
-    console.log("No one wins, but no one loses either, ggs")
+    console.log("No one wins, but no one loses either")
 }
 else if (humanScore>computerScore){
     console.log("Nice job! You won the whole tournament!")
@@ -58,6 +59,3 @@ else if (humanScore>computerScore){
 else{
     console.log("The computer won, oh well, better luck next time!")
 }
-}
-}
-console.log(playGame())
