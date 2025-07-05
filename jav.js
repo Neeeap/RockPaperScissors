ab7bd4b04a8fa73fe5c74b898792fdcd2eef5d73
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-07-05 12:11:00 +0700

    made indentation changes

--- jav.js
+++ jav.js
@@ -1,6 +1,6 @@
 let humanScore=0
 let computerScore=0
-function getComputerChoice(){
+function getComputerChoice() {
     choice=Math.floor(Math.random()*100)
     choice <= 33
     ? choice="Rock"
@@ -9,53 +9,55 @@ function getComputerChoice(){
     : choice = "Scissors"
     return choice
 }
-function getPlayerChoice(){
+function getPlayerChoice() {
     choice=prompt("Choose rock, paper, or scissors")
-    while (true){
-    if (choice.toLowerCase()==="rock"){
+    while (true) {
+    if (choice.toLowerCase()==="rock") {
         choice="Rock"
         break
-    }
-    else if (choice.toLowerCase()==="paper"){
+        }
+    else if (choice.toLowerCase()==="paper") {
         choice = "Paper"
         break
-    }
+        }
     else if (choice.toLowerCase()==="scissors") {
         choice = "Scissors"
         break
-    }
+        }
     else {
         choice=prompt("Invalid, try again")
-    }
+        }
     }
     return choice
 }
-function playRound(getComputerChoice,getPlayerChoice){
-    if (getComputerChoice===getPlayerChoice){
+function playRound(getComputerChoice,getPlayerChoice) {
+    if (getComputerChoice===getPlayerChoice) {
         console.log("It's a tie!")
     }
-    else if (getComputerChoice === "Paper" && getPlayerChoice==="Rock" || getComputerChoice === "Rock" && getPlayerChoice==="Scissors" || getComputerChoice === "Scissors" && getPlayerChoice==="Paper") {
+    else if (getComputerChoice === "Paper" && getPlayerChoice==="Rock" ||
+        getComputerChoice === "Rock" && getPlayerChoice==="Scissors" ||
+        getComputerChoice === "Scissors" && getPlayerChoice==="Paper") {
         console.log("Computer wins!")
         ++computerScore
     }
-    else{
+    else {
         console.log("You win!")
         ++humanScore
     }
     console.log(`${humanScore} - ${computerScore}`)
 }
-function playGame(){
-for (let i =1; i<=5; i++){
+function playGame() {
+for (let i =1; i<=5; i++) {
 playRound(getComputerChoice(),getPlayerChoice())
-}
+    }
 }
 playGame()
-if (humanScore===computerScore){
+if (humanScore===computerScore) {
     console.log("No one wins, but no one loses either")
 }
-else if (humanScore>computerScore){
+else if (humanScore>computerScore) {
     console.log("Nice job! You won the whole tournament!")
 }
-else{
+else {
     console.log("The computer won, oh well, better luck next time!")
 }
