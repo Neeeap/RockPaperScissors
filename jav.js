8e23792faf5be9ee3fd08389f18e3812eac494d0
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-07-14 15:44:14 +0700

    made ui look better

--- jav.js
+++ jav.js
@@ -14,10 +14,10 @@ choices.addEventListener("click", (a) =>{
     let chose=a.target;
     switch(chose.id){
         case "rock":
-            playRound(getComputerChoice(),"Paper");
+            playRound(getComputerChoice(),"Rock");
             break;
         case "paper":
-            playRound(getComputerChoice(),"Rock");
+            playRound(getComputerChoice(),"Paper");
             break;
         case "scissors":
             playRound(getComputerChoice(),"Scissors");
@@ -25,28 +25,32 @@ choices.addEventListener("click", (a) =>{
     }
 })
 let res=document.querySelector("#results")
+let updateScore=document.querySelector("#score")
+let ann=document.querySelector("#announce")
+let play=document.querySelector("#player")
+let computer=document.querySelector("#comp")
 function playRound(getComputerChoice,getPlayerChoice) {
+    play.textContent=`You chose: ${getPlayerChoice}`
+    computer.textContent=`Computer chose: ${getComputerChoice}`
     if (getComputerChoice===getPlayerChoice) {
-        console.log("It's a tie!")
+        ann.textContent="It's a tie!"
     }
     else if (getComputerChoice === "Paper" && getPlayerChoice==="Rock" ||
         getComputerChoice === "Rock" && getPlayerChoice==="Scissors" ||
         getComputerChoice === "Scissors" && getPlayerChoice==="Paper") {
-        console.log("Computer wins!")
+        ann.textContent="Computer wins!"
         ++computerScore
     }
     else {
-        console.log("You win!")
+        ann.textContent="You win!"
         ++humanScore
     }
-    console.log(`${humanScore} - ${computerScore}`)
+    updateScore.textContent=`${humanScore} - ${computerScore}`
+    let won=document.querySelector("#winner")
+if (humanScore==5) {
+    won.textContent="Nice job! You won the whole tournament!"
 }
-if (humanScore===computerScore) {
-    console.log("No one wins, but no one loses either")
+else if (computerScore==5) {
+    won.textContent="The computer won, oh well, better luck next time!"
 }
-else if (humanScore>computerScore) {
-    console.log("Nice job! You won the whole tournament!")
-}
-else {
-    console.log("The computer won, oh well, better luck next time!")
 }
